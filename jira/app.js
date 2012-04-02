538f1e92ee451ea9e1b57e7730d5c51d7e425494
James A. Rosen <james@zendesk.com>
2012-04-02 13:26:59 -0700

    apps' app.js returns the app instead of setting it

--- jira/app.js
+++ jira/app.js
@@ -1,8 +1,6 @@
-//= require ./app.js
-
 (function() {
 
-  ZendeskApps.JiraApp = ZendeskApps.App.extend({
+  return ZendeskApps.App.extend({
     location: ZendeskApps.Site.TICKET_PROPERTIES,
     appID: '/apps/01-jira/versions/1.0.0',
     name: 'Jira',
