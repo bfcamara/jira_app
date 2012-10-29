e84c6f4c777a68b51ad94beeefd8d0d5506bc659
James A. Rosen <james@zendesk.com>
2012-10-29 15:38:36 -0700

    introduce loading state

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 (function() {
 
   return {
-    defaultState: 'unshared'
+    defaultState: 'loading',
+
+    events: {
+      'app.activated': 'onActivated'
+    },
+
+    onActivated: function() {
+      this.switchTo('unshared');
+    }
   };
 
 }());
