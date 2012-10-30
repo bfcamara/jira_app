2f4270e2c7734f7a628f3ebb9de0736f088cae30
James A. Rosen <james@zendesk.com>
2012-10-29 17:24:04 -0700

    fetch & render list of projects

--- app.js
+++ app.js
@@ -5,22 +5,41 @@
 
     events: {
       'app.activated':              'checkSharedWith',
-      'ticket.sharedWith.changed':  'checkSharedWith'
+      'ticket.sharedWith.changed':  'checkSharedWith',
+
+      'fetchProjects.done':         'onProjectsFetched'
+    },
+
+    requests: {
+      fetchProjects: function(agreementID) {
+        return {
+          url: helpers.fmt('/sharing_agreements/%@/jira_projects', agreementID)
+        };
+      }
     },
 
     checkSharedWith: function() {
-      if ( this.sharedWithJIRA() ) {
-        this.switchTo('share');
+      var agreementID = this.sharedWithJiraId();
+      if ( agreementID != null ) {
+        this.ajax('fetchProjects', agreementID);
       } else {
         this.switchTo('unshared');
       }
     },
 
-    sharedWithJIRA: function() {
+    sharedWithJiraId: function() {
       var sharedWith = this.ticket().sharedWith();
-      return sharedWith !== null &&
-             sharedWith.length === 1 &&
-             sharedWith[0].partnerName() === 'jira';
+      if ( sharedWith == null ||
+           sharedWith.length !== 1 ||
+           sharedWith[0].partnerName() !== 'jira' ) {
+        return null;
+      }
+      return sharedWith[0].id();
+    },
+
+    onProjectsFetched: function(projects) {
+      this.projects = projects;
+      this.switchTo('share', this);
     }
   };
 
