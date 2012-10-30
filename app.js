3122aa5539b07d05498d3e04b8e1fd0d36969438
James A. Rosen <james@zendesk.com>
2012-10-30 10:09:05 -0700

    share form: render story type options on project change

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
     defaultState: 'loading',
 
     events: {
-      'app.activated':              'checkSharedWith',
-      'ticket.sharedWith.changed':  'checkSharedWith',
+      'app.activated':                    'checkSharedWith',
+      'ticket.sharedWith.changed':        'checkSharedWith',
 
-      'fetchProjects.done':         'onProjectsFetched'
+      'fetchProjects.done':               'onProjectsFetched',
+      'change select[name="project_id"]': 'onProjectSelected'
     },
 
     requests: {
@@ -40,6 +41,36 @@
     onProjectsFetched: function(projects) {
       this.projects = projects;
       this.switchTo('share', this);
+    },
+
+    onProjectSelected: function(e) {
+      this.selectedProjectID = this.$(e.target).val();
+      this.$('select[name="story_type_id"]')
+          .html( '' + this.storyTypeOptions() );
+    },
+
+    storyTypesForCurrentProject: function() {
+      if ( this.projects == null || this.selectedProjectID == null ) {
+        return [];
+      }
+      var project = _.find(this.projects, function(p) {
+        return p.id === this.selectedProjectID;
+      }, this);
+      return project == null ? [] : project.issueTypes;
+    },
+
+    storyTypeOptions: function() {
+      // This should really be a template, but Lotus's current version of
+      // Handlebars (1.0.beta.2) can't render templates that end in
+      // {{#foos}}...{{/foos}} blocks. As soon as Lotus upgrades to Ember 1.0,
+      // it can upgrade Handlebars, and we can replace this with a template.
+      var optionTemplate = '<option value="%@">%@</option>',
+          result = '';
+      result += helpers.fmt(optionTemplate, '', this.I18n.t('share.story_type.prompt'));
+      this.storyTypesForCurrentProject().forEach(function(type) {
+        result += helpers.fmt(optionTemplate, type.id, type.name);
+      });
+      return helpers.safeString( result );
     }
   };
 
